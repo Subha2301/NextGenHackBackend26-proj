134b86c50754d637c0fe35074a3e3fc2ca78c7a1
Subhasis Roy <subhasisroy947@gmail.com>
2026-03-02 12:19:33 +0530

    msg

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,16 +1,19 @@
 const jwt = require("jsonwebtoken");
 
-exports.protect = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
+module.exports = function (req, res, next) {
+  const authHeader = req.header("Authorization");
 
-  if (!token)
-    return res.status(401).json({ message: "Not authorized" });
+  if (!authHeader) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  const token = authHeader.replace("Bearer ", "");
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
-  } catch (error) {
+  } catch (err) {
     res.status(401).json({ message: "Invalid token" });
   }
 };
