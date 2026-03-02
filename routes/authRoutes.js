fcd52e173af266a54c31c046a844eb01b7434d55
Subhasis Roy <subhasisroy947@gmail.com>
2026-03-02 17:20:30 +0530

    msg1

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const { register } = require("../controllers/authController"); // import function
 
-router.post("/register", (req, res) => {
-  res.send("Register working");
-});
+router.post("/register", register); // second argument must be function
 
-module.exports = router;  
+module.exports = router;
