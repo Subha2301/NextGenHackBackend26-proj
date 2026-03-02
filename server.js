9c763d04269b69cea3e8ec1114ae80f26ce9fe6d
Subhasis Roy <subhasisroy947@gmail.com>
2026-03-02 11:49:15 +0530

    msg

--- server.js
+++ server.js
@@ -1,24 +1,8 @@
-// require("dotenv").config();
-// const express = require("express");
-// const connectDB = require("./config/db");
-
-// const app = express();
-
-// connectDB();
-
-// app.use(express.json());
-
-// app.listen(process.env.PORT || 5000, () =>
-//   console.log("Server running")
-// );
-
-
-
-
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
 const complaintRoutes = require("./routes/complaintRoutes");
+const authRoutes = require("./routes/authRoutes");
 const cors = require("cors");
 
 const app = express();
@@ -31,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
+app.use("/api/auth", authRoutes);
 app.use("/api/complaints", complaintRoutes);
 
 // Optional homepage route
